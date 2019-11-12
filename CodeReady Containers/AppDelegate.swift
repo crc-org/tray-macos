1040b052549ac74c133dd079137b807cf711dbff
Anjan Nath <kaludios@gmail.com>
2019-11-12 14:51:28 +0530

    Initial Commit

---
+++ CodeReady Containers/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CodeReady Containers
+//
+//  Created by Anjan Nath on 12/11/19.
+//  Copyright © 2019 Red Hat. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CodeReady Containers/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CodeReady Containers
+//
+//  Created by Anjan Nath on 12/11/19.
+//  Copyright © 2019 Red Hat. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
