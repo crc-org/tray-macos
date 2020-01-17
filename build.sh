276e21e27de36e36549cd8c5e413ea5a07fd0703
Anjan Nath <kaludios@gmail.com>
2020-01-17 14:53:14 +0530

    Add build.sh script to make final app archive

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+BASE_DIR=$(pwd)
+BUILD_DIR=$BASE_DIR/out
+WORKSPACE=$BASE_DIR/CodeReady\ Containers.xcworkspace
+ARCHIVE_PATH=$BUILD_DIR/CodeReady_Containers.xcarchive
+EXPORT_OPTIONS=$BASE_DIR/CodeReady\ Containers/ExportOptions.plist
+SCHEME=CodeReady\ Containers
+
+if [ ! -f $BUILD_DIR ]; then
+	mkdir -p $BUILD_DIR
+fi
+
+# Build xcarchive
+xcodebuild -workspace "$WORKSPACE" -config Release -scheme "$SCHEME" -archivePath "$ARCHIVE_PATH" archive
+
+# Build .app archive
+xcodebuild -exportArchive -archivePath "$ARCHIVE_PATH" -exportPath "$BUILD_DIR" -exportOptionsPlist "$EXPORT_OPTIONS"
