de0a66e1c66f026246271f96cd8d5556cf8a8533
Anjan Nath <kaludios@gmail.com>
2020-02-17 17:50:27 +0530

    Issue #11 Synchronize menu states
    Update menu item states after each start/stop/delete
    operation.

--- CodeReady Containers/DaemonCommander/Handlers.swift
+++ CodeReady Containers/DaemonCommander/Handlers.swift
@@ -60,7 +60,14 @@ func HandleStop() {
     if String(bytes: data, encoding: .utf8) == "Failed" {
         DispatchQueue.main.async {
             let appDelegate = NSApplication.shared.delegate as? AppDelegate
-            appDelegate?.updateMenuStates(state: MenuStates(startMenuEnabled: true, stopMenuEnabled: false, deleteMenuEnabled: true, webconsoleMenuEnabled: true, ocLoginForDeveloperEnabled: true, ocLoginForAdminEnabled: true, copyOcLoginCommand: true))
+            appDelegate?.updateMenuStates(state: MenuStates(startMenuEnabled: true,
+                                                            stopMenuEnabled: false,
+                                                            deleteMenuEnabled: true,
+                                                            webconsoleMenuEnabled: true,
+                                                            ocLoginForDeveloperEnabled: true,
+                                                            ocLoginForAdminEnabled: true,
+                                                            copyOcLoginCommand: true)
+            )
             
             showAlertFailedAndCheckLogs(message: "Failed deleting the CRC cluster", informativeMsg: "Make sure the CRC daemon is running, or check the logs to get more information")
         }
@@ -71,10 +78,32 @@ func HandleStop() {
         if stopResult.Success {
             DispatchQueue.main.async {
                 let appDelegate = NSApplication.shared.delegate as? AppDelegate
-                appDelegate?.updateMenuStates(state: MenuStates(startMenuEnabled: true, stopMenuEnabled: false, deleteMenuEnabled: true, webconsoleMenuEnabled: false, ocLoginForDeveloperEnabled: false, ocLoginForAdminEnabled: false, copyOcLoginCommand: false))
+                appDelegate?.updateMenuStates(state: MenuStates(startMenuEnabled: true,
+                                                                stopMenuEnabled: false,
+                                                                deleteMenuEnabled: true,
+                                                                webconsoleMenuEnabled: false,
+                                                                ocLoginForDeveloperEnabled: false,
+                                                                ocLoginForAdminEnabled: false,
+                                                                copyOcLoginCommand: false)
+                )
                 appDelegate?.updateStatusMenuItem(status: "Stopped")
                 
                 displayNotification(title: "Successfully Stopped Cluster", body: "The CRC Cluster was successfully stopped")
+            }
+        }
+        if stopResult.Error != "" {
+            DispatchQueue.main.async {
+                showAlertFailedAndCheckLogs(message: "Failed to stop OpenShift cluster", informativeMsg: "\(stopResult.Error)")
+                let appDelegate = NSApplication.shared.delegate as? AppDelegate
+                appDelegate?.showClusterStatusUnknownOnStatusMenuItem()
+                appDelegate?.updateMenuStates(state: MenuStates(startMenuEnabled: false,
+                                                                stopMenuEnabled: true,
+                                                                deleteMenuEnabled: true,
+                                                                webconsoleMenuEnabled: true,
+                                                                ocLoginForDeveloperEnabled: true,
+                                                                ocLoginForAdminEnabled: true,
+                                                                copyOcLoginCommand: true)
+                )
             }
         }
     } catch let jsonErr {
@@ -95,29 +124,58 @@ func HandleStart() {
         // Adjust the menus
         DispatchQueue.main.async {
             let appDelegate = NSApplication.shared.delegate as? AppDelegate
-            appDelegate?.updateMenuStates(state: MenuStates(startMenuEnabled: true, stopMenuEnabled: false, deleteMenuEnabled: true, webconsoleMenuEnabled: false, ocLoginForDeveloperEnabled: false, ocLoginForAdminEnabled: false, copyOcLoginCommand: false))
+            appDelegate?.updateMenuStates(state: MenuStates(startMenuEnabled: true,
+                                                            stopMenuEnabled: false,
+                                                            deleteMenuEnabled: true,
+                                                            webconsoleMenuEnabled: false,
+                                                            ocLoginForDeveloperEnabled: false,
+                                                            ocLoginForAdminEnabled: false,
+                                                            copyOcLoginCommand: false))
             
             showAlertFailedAndCheckLogs(message: "Failed to start OpenShift cluster", informativeMsg: "CodeReady Containers failed to start the OpenShift cluster, ensure the CRC daemon is running or check the logs to find more information")
         }
+    } else {
+        displayNotification(title: "CodeReady Containers", body: "Starting OpenShift Cluster, this could take a few minutes..")
     }
     do {
         let startResult = try JSONDecoder().decode(StartResult.self, from: data)
-        if startResult.KubeletStarted {
+        if startResult.Status == "Running" {
             DispatchQueue.main.async {
                 let appDelegate = NSApplication.shared.delegate as? AppDelegate
-                appDelegate?.updateStatusMenuItem(status: startResult.Status)
-                appDelegate?.updateMenuStates(state: MenuStates(startMenuEnabled: false, stopMenuEnabled: true, deleteMenuEnabled: true, webconsoleMenuEnabled: true, ocLoginForDeveloperEnabled: true, ocLoginForAdminEnabled: true, copyOcLoginCommand: true))
-                
-                displayNotification(title: "Cluster started", body: "CodeReady Containers OpenShift Cluster is up")
+                appDelegate?.showClusterStartingMessageOnStatusMenuItem()
+                appDelegate?.updateMenuStates(state: MenuStates(startMenuEnabled: false, stopMenuEnabled: true, deleteMenuEnabled: true, webconsoleMenuEnabled: false, ocLoginForDeveloperEnabled: false, ocLoginForAdminEnabled: false, copyOcLoginCommand: false))
+            }
+            // if vm is running but kubelet not yet started
+            if !startResult.KubeletStarted {
+                DispatchQueue.main.async {
+                    let appDelegate = NSApplication.shared.delegate as? AppDelegate
+                    appDelegate?.showClusterStatusUnknownOnStatusMenuItem()
+                    displayNotification(title: "CodeReady Containers", body: "CodeReady Containers OpenShift Cluster is taking longer to start")
+                }
+            } else {
+                DispatchQueue.main.async {
+                    let appDelegate = NSApplication.shared.delegate as? AppDelegate
+                    appDelegate?.updateStatusMenuItem(status: startResult.Status)
+                    appDelegate?.updateMenuStates(state: MenuStates(startMenuEnabled: false, stopMenuEnabled: true, deleteMenuEnabled: true, webconsoleMenuEnabled: true, ocLoginForDeveloperEnabled: true, ocLoginForAdminEnabled: true, copyOcLoginCommand: true))
+                    
+                    displayNotification(title: "CodeReady Containers", body: "OpenShift Cluster is running")
+                }
             }
         }
-        if !startResult.KubeletStarted {
+        if startResult.Error != "" {
             DispatchQueue.main.async {
+                var errMsg = startResult.Error.split(separator: "\n")
+                showAlertFailedAndCheckLogs(message: "Failed to start OpenShift cluster", informativeMsg: "\(errMsg[0])")
                 let appDelegate = NSApplication.shared.delegate as? AppDelegate
-                appDelegate?.updateStatusMenuItem(status: startResult.Status)
-                appDelegate?.updateMenuStates(state: MenuStates(startMenuEnabled: false, stopMenuEnabled: true, deleteMenuEnabled: true, webconsoleMenuEnabled: true, ocLoginForDeveloperEnabled: true, ocLoginForAdminEnabled: true, copyOcLoginCommand: true))
-                
-                displayNotification(title: "Cluster is still starting", body: "CodeReady Containers OpenShift Cluster is taking longer to start")
+                appDelegate?.showClusterStatusUnknownOnStatusMenuItem()
+                appDelegate?.updateMenuStates(state: MenuStates(startMenuEnabled: false,
+                                                                stopMenuEnabled: true,
+                                                                deleteMenuEnabled: true,
+                                                                webconsoleMenuEnabled: false,
+                                                                ocLoginForDeveloperEnabled: false,
+                                                                ocLoginForAdminEnabled: false,
+                                                                copyOcLoginCommand: false)
+                )
             }
         }
     } catch let jsonErr {
@@ -147,8 +205,15 @@ func HandleDelete() {
             DispatchQueue.main.async {
                 let appDelegate = NSApplication.shared.delegate as? AppDelegate
                 appDelegate?.updateMenuStates(state: MenuStates(startMenuEnabled: true, stopMenuEnabled: false, deleteMenuEnabled: false, webconsoleMenuEnabled: false, ocLoginForDeveloperEnabled: false, ocLoginForAdminEnabled: false, copyOcLoginCommand: false))
-                
+                appDelegate?.showClusterStatusUnknownOnStatusMenuItem()
                 displayNotification(title: "Cluster Deleted", body: "The CRC Cluster is successfully deleted")
+            }
+        }
+        if deleteResult.Error != "" {
+            DispatchQueue.main.async {
+                showAlertFailedAndCheckLogs(message: "Failed to delete OpenShift cluster", informativeMsg: "\(deleteResult.Error)")
+                let appDelegate = NSApplication.shared.delegate as? AppDelegate
+                appDelegate?.showClusterStatusUnknownOnStatusMenuItem()
             }
         }
     } catch let jsonErr {
