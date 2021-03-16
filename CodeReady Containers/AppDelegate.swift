27b189b6ed554e0f4d821cc4b5669ad200f1692a
Anjan Nath <kaludios@gmail.com>
2021-03-16 13:19:45 +0530

    Fix detection of if another instance of the app is running
    Show alert if another instance of the app is running and terminate

--- CodeReady Containers/AppDelegate.swift
+++ CodeReady Containers/AppDelegate.swift
@@ -55,13 +55,19 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
         }
         menu.delegate = self
         statusItem.menu = self.menu
-
-        let applications = NSWorkspace.shared.runningApplications
-
-        for app in applications where app == NSWorkspace.shared.self {
-            NSApplication.shared.terminate(self)
+        
+        if NSRunningApplication.runningApplications(withBundleIdentifier: Bundle.main.bundleIdentifier!).count > 1 {
+            print("more than one running")
+            let alert = NSAlert()
+            alert.addButton(withTitle: "OK")
+            alert.messageText = "Another instance of \(Bundle.main.bundleIdentifier!) is running."
+            alert.informativeText = "This instance will now terminate."
+            alert.alertStyle = .critical
+            alert.runModal()
+            
+            NSApp.terminate(self)
         }
-
+        
         let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options: [.alert, .sound], completionHandler: { (granted, error) in
             notificationAllowed = granted
